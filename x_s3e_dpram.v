c40258c268bf1b19ad2d7e68a4eb09e18bf24d37
Bai Jinyu <baijinyu0053@sina.com>
2017-04-04 16:41:49 +0800

    double port ram

---
+++ x_s3e_dpram.v
@@ -0,0 +1,52 @@
+`timescale 1ns/1ns
+`include "global_config.h"
+`include "stddef.h"
+`include "nettype.h"
+module x_s3e_dpram(clka,wea,addra,dina,douta,
+				clkb,web,addrb,dinb,doutb);
+	
+	input clka,clkb;
+	input wea,web;
+	input[11:0] addra,addrb;
+	input[31:0] dina,dinb;
+	
+	output reg[31:0] douta,doutb;
+	
+	reg[31:0] mem[4095:0];
+	
+	always@(posedge clka)
+	begin
+		if((web == `ENABLE) && (addra == addrb))
+		begin
+			douta <= #1 dinb;
+		end
+		else
+		begin
+			douta <= #1 mem[addra];
+		end
+		
+		if(wea == `ENABLE)
+		begin
+			mem[addra] <= #1 dina;
+		end
+	end
+	
+	always@(posedge clkb)
+	begin
+		if((wea == `ENABLE) && (addrb == addra))
+		begin
+			doutb <= #1 dina;
+		end
+		else
+		begin
+			doutb <= #1 mem[addrb];
+		end
+		
+		if(web == `ENABLE)
+		begin
+			mem[addrb] <= #1 dinb;
+		end
+	end
+
+endmodule
+	
