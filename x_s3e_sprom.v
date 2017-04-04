6f4f7e91d38d89b8f72294ca432b083db14eb745
Bai Jinyu <baijinyu0053@sina.com>
2017-04-04 16:19:24 +0800

    single port rom

---
+++ x_s3e_sprom.v
@@ -0,0 +1,14 @@
+`timescale 1ns/1ns
+module x_s3e_sprom(clka,addra,douta);
+	input clka;
+	input[10:0] addra;
+	output reg[31:0] douta;
+	
+	reg[31:0] mem[2047:0];
+	
+	always@(posedge clka)
+	begin
+		douta <= #1 mem[addra];
+	end
+endmodule
+	
